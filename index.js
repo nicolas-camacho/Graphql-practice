813668643e018a62558483e6e08b4c756edbc81b
nicolas-camacho <ncamacho1@cuc.edu.co>
2019-04-18 01:17:52 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const express_graphql = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+	type Query {
+		message: String
+	}
+`)
+
+const root = {
+	message: ()=> "Hello World!"
+}
+
+app.use('/graphql', express_graphql({
+	schema: schema,
+	rootValue: root,
+	graphiql: true
+}))
+
+app.listen(3000, () => console.log('server on port 3000'))
